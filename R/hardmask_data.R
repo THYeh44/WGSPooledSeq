c8be44ec42b7eff84d17c91337e8a735b0b1908e
Tsunghan Yeh <thyeh11677@gmail.com>
2023-06-06 14:52:54 -0700

    Adding duplication removal option for the pooled analysis

---
+++ R/hardmask_data.R
@@ -0,0 +1,13 @@
+#' C. elegans hardmask list
+#'
+#' A summary lsit containing all the C. elegans duplicated seuqeunce (hardmasked) postions based on PRJNA13758.WBPS18.
+#'
+#' @format The variables are as follows:
+#'  \describe{
+#'      \item CHR Chromosome.
+#'      \item Seq Seuqence position.
+#' }
+#' @source {WormBase, Parasite}
+#' @examples
+#' data(hardmask_list)    # Lazy loading
+"hardmask_list"
